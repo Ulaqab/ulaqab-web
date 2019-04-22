c2a83bd450138a5d28225cfa64ced2c196ae2a86
Yajun Wang <ayac3j@gmail.com>
2019-04-22 17:30:31 +0800

    add config proxy

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,27 @@ module.exports = {
                 `
       }
     }
-  }
+  },
+
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://172.31.100.217:9000",
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          "^/api": "/api"
+        }
+      },
+      "/img": {
+        target: "http://172.31.100.217:9000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/img": "/img"
+        }
+      }
+    }
+  },
+
+  lintOnSave: undefined
 };
